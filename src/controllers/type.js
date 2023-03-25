a94486ceb2afc54a1d0da5fe25450504e2595bb3
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-03-25 20:32:49 -0300

    Update type.js

--- src/controllers/type.js
+++ src/controllers/type.js
@@ -1,25 +1,21 @@
 const { Type } = require('../db');
 const axios = require('axios');
 
-const getAllTypes = async (req, res) => {
-      const typesBD = await Type.findAll();
-
-if(!typeDb.length){ 
-try { 
-  
-
-    const typeUrl = await axios.get('https://pokeapi.co/api/v2/type/')
-    const types = typeUrl.data.results.map(t => t.name);
-    const typesC = types.map(async t => await Type.create({ name: t})); 
-    const allTypes = await Type.findAll();
-    res.status(200).send(allTypes); 
-    } catch (error) {
-    res.status(400).json('ups algo salio mal')
-    }
-   } else {
+const getAllTypes = async ( req, res ) => {
+    const typeDb = await Type.findAll();
+    if(!typeDb.length) {
+        try {
+            const typesUrl = await axios.get("https://pokeapi.co/api/v2/type"); 
+            const types = typesUrl.data.results.map(t => t.name);
+            const typesC = types?.map(async t => await Type.create({ name: t})); 
+            const allTypes = await Type.findAll();
+            res.status(200).send(allTypes); 
+        } catch (error) {
+            res.status(404).send({error: "error"});
+        }; 
+    } else {
         res.status(200).send(typeDb);
-
-};
+}
 
 module.exports = {
     getAllTypes
